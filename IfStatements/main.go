57f6564b36278e2538483db7785c9438539ce012
Kye <kyleofficalhd@gmail.com>
2021-03-08 09:33:54 -0500

    Added if statements project

---
+++ IfStatements/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 11
+
+	if x < 2 {
+		fmt.Println("2 >", x)
+	} else if x > 9 {
+		fmt.Println("9 <", x)
+	} else {
+		fmt.Println("failed to be less than 2 or greater than 9 for value", x)
+	}
+}
